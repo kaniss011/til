b209d87c595f777a0b0bc0b8bd22bf49ed4c4615
katniss011 <katnissski@gmail.com>
2024-11-13 08:55:23 +0900

    JS 작업

---
+++ script.js
@@ -0,0 +1,73 @@
+const isMember = false;
+console.log(typeof isMember, isMember);
+
+const isLogin = true;
+console.log(typeof isLogin, isLogin);
+// ============================================= //
+const isNow = "12시";
+console.log("12시", typeof isNow, isNow);
+
+if (isNow) {
+  console.log("true 입니다.");
+} else {
+  console.log("false 입니다.");
+}
+
+const isPlay = 100;
+console.log(typeof isPlay, isPlay);
+
+if (isPlay) {
+  console.log("isPlay true 입니다.");
+} else {
+  console.log("isPlay false 입니다.");
+}
+
+const isPoint = 0;
+console.log(typeof isPoint, isPoint);
+
+if (isPoint) {
+  console.log("isPoint true 입니다.");
+} else {
+  console.log("isPoint false 입니다.");
+}
+
+const isTeam = "";
+console.log(typeof isTeam, isTeam);
+
+if (isTeam) {
+  console.log("isTeam true 입니다.");
+} else {
+  console.log("isTeam false 입니다.");
+}
+
+const isItems = [];
+console.log(typeof isItems, isItems);
+if (isItems) {
+  console.log("isItems true 입니다.");
+} else {
+  console.log("isItems false 입니다.");
+}
+
+const isInfo = {};
+console.log(typeof isInfo, isInfo);
+if (isInfo) {
+  console.log("isInfo true 입니다.");
+} else {
+  console.log("isInfo false 입니다.");
+}
+
+const isUn = undefined;
+console.log(typeof isUn, isUn);
+if (isUn) {
+  console.log("isUn true 입니다.");
+} else {
+  console.log("isUn false 입니다.");
+}
+
+const isN = null;
+console.log(typeof isN, isN);
+if (isN) {
+  console.log("isN true 입니다.");
+} else {
+  console.log("isN false 입니다.");
+}
